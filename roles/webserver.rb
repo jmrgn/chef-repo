3db53fe7dd81edc7e59fed95f84085fb165380b7
James Morgan <james.morgan@rackspace.com>
2015-02-24 14:53:05 -0500

    Basic roles

---
+++ roles/webserver.rb
@@ -0,0 +1,15 @@
+name "webserver"
+description "Web Server"
+run_list "role[base]", "recipe[iis_demo]"
+default_attributes({
+  "iis_demo" => {
+    "sites" => {
+      "admin" => {
+        "port" => 8000
+      },
+      "bears" => {
+        "port" => 8081
+      }
+    }
+  }
+})
